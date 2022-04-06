a8aa88779247a734fae9c0fb623a87ce1cde488b
namvt48 <vuthenam.9.3.4@gmail.com>
2022-04-06 22:08:50 +0700

    Sign in screen

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,134 +1,118 @@
-
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-// import 'package:google_fonts/google_fonts.dart';
+import 'package:mobile/assets.dart';
+import 'package:mobile/widgets/widgets.dart';
 
-class LoginScreen extends StatefulWidget {
-  @override
-  _LogInScreenState createState() => _LogInScreenState();
-}
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
 
-class _LogInScreenState extends State<LoginScreen> {
-  Color whiteclr = Colors.white;
-  Color blackclr = Colors.black;
+  Widget _helpButton() {
+    return FlatButton(
+      onPressed: () => {print("need help")},
+      child: const Text(
+        'Need help?',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 17,
+        ),
+      ),
+    );
+  }
+
+  Widget _signupButton() {
+    return FlatButton(
+      onPressed: () => {print("sign up")},
+      child: const Text(
+        'New to Netflix? Sign up now.',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 17,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    double h = MediaQuery.of(context).size.height;
-    double w = MediaQuery.of(context).size.width;
-
+    Size size = MediaQuery.of(context).size;
 
     return Scaffold(
-      backgroundColor: blackclr,
-      body: Column(
-        children: <Widget>[
-          SafeArea(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.only(left: 15),
-                    child: IconButton(
-                      onPressed: (){Navigator.of(context).pop();},
-                      icon: Icon(
-                        Icons.arrow_back_ios,
-                        color: whiteclr,
-                      ),
-                    ),
-                  ),
-
-                  Padding(
-                    padding: const EdgeInsets.only(right: 30),
-                    child: Text(
-                      'Help',
-                      // style: GoogleFonts.rubik(
-                      //     fontSize: 18,
-                      //     color: Colors.white,
-                      //     fontWeight: FontWeight.w700),
-                    ),
-                  )
-                ],
-              )),
-          SizedBox(height: h/3.5,),
-          Container(
-            width: w/1.1,
-            height: h*.07,
-            decoration: BoxDecoration(color: Colors.grey[700],borderRadius: BorderRadius.circular(5)),
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text('Email or phone number',
-                // style:GoogleFonts.rubik(
-                //   fontSize: 20,
-                //   color: Colors.white,
-                //   fontWeight: FontWeight.w400),
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios),
+          color: Colors.white,
+          onPressed: () => print("back"),
+        ),
+        title: Image.asset(
+          Assets.netflixLogo1,
+          width: 106,
+          height: 33,
+          fit: BoxFit.cover,
+        ),
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: size.height * 0.14,
+            ),
+            InputField(
+              onChange: (value) {},
+            ),
+            PasswordField(
+              onChange: (value) {},
+            ),
+            const SignInButton(),
+            _helpButton(),
+            _signupButton(),
+            const Text(
+              'Sign in is protected by Google reCAPTCHA to ensure you\'re not a bot',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 12.0,
               ),
             ),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
-          ),
-          SizedBox(height: h/35,),
-          Container(
-            width: w/1.1,
-            height: h*.07,
-            decoration: BoxDecoration(color: Colors.grey[700],borderRadius: BorderRadius.circular(5)),
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Text('Password',
-                    // style:GoogleFonts.rubik(
-                    //   fontSize: 20,
-                    //   color: Colors.white,
-                    //   fontWeight: FontWeight.w400),
-                  ),
-                  Text('SHOW',
-                    // style:GoogleFonts.rubik(
-                    //   fontSize: 20,
-                    //   color: Colors.white,
-                    //   fontWeight: FontWeight.w400),
-                  ),
-                ],
+class SignInButton extends StatelessWidget {
+  const SignInButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20),
+      child: SizedBox(
+        width: size.width * 0.9,
+        height: 60,
+        child: OutlinedButton(
+          onPressed: () => print("login"),
+          style: ButtonStyle(
+            side: MaterialStateProperty.all(
+              const BorderSide(
+                color: Colors.grey,
+                width: 2.0,
+                style: BorderStyle.solid,
               ),
             ),
-
           ),
-          SizedBox(height: h/30,),
-          Container(
-            width: w/1.1,
-            height: h*.07,
-            decoration: BoxDecoration(color: Colors.black,borderRadius: BorderRadius.circular(5)),
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Center(
-                child: Text('Sign In',
-                  // style:GoogleFonts.rubik(
-                  //   fontSize: 20,
-                  //   color: Colors.white,
-                  //   fontWeight: FontWeight.w600),
-                ),
-              ),
+          child: const Text(
+            "Sign In",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 22,
             ),
-
           ),
-          SizedBox(height: h/30,),
-          Container(
-            width: w/1.1,
-            height: h*.07,
-            decoration: BoxDecoration(color: Colors.black,borderRadius: BorderRadius.circular(5)),
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Center(
-                child: Text('Recover Password',
-                  // style:GoogleFonts.rubik(
-                  //   fontSize: 18,
-                  //   color: Colors.white,
-                  //   fontWeight: FontWeight.w600),
-                ),
-              ),
-            ),
-
-          ),
-
-        ],
+        ),
       ),
     );
   }
