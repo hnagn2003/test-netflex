c1a2da7112b6f5e4db395aa54473cb0be3f3e39c
namvt48 <vuthenam.9.3.4@gmail.com>
2022-04-11 20:49:23 +0700

    Add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mobile/screens/screens.dart';
+import 'package:mobile/screens/splash.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,21 +16,12 @@ class MyApp extends StatelessWidget {
       title: 'Netflix',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
         scaffoldBackgroundColor: Colors.black,
         fontFamily: 'Netflix Sans',
       ),
-      home: const LoginScreen(),
+      home: const Splash(),
     );
   }
 }
